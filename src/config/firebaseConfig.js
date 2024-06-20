9a21e3c850dc6217cf34a2d519def73819024c2b
anujXthunder <anujmgr2003@gmail.com>
2024-06-20 15:44:49 +0530

    Initial commit

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -3,9 +3,9 @@
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCJ8GLwaDw8xAYe67m5bsOtkBcmeDkoyYA",
-  authDomain: "cryptopulse-1bb80.firebaseapp.com",
-  projectId: "cryptopulse-1bb80",
-  storageBucket: "cryptopulse-1bb80.appspot.com",
+  authDomain: "cryptrack-1bb80.firebaseapp.com",
+  projectId: "cryptrack-1bb80",
+  storageBucket: "cryptrack-1bb80.appspot.com",
   messagingSenderId: "139762123902",
   appId: "1:139762123902:web:d1dd4b52de72f7f3d6442f"
 };
